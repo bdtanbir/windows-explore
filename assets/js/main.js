0e99a35844e11ebabf7a48bf56d411d80b5df30e
Jummanbd <tanbirsylhet20@gmail.com>
2021-08-01 16:00:02 +0600

    finished drag and resize option

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,157 @@
 
 
 
-// var element = document.querySelectorAll(".windows-header-nav-menu li");
-// var update_num = document.querySelector(".update-num");
-// update_num.innerHTML = element.length
+var folders = document.querySelectorAll(".windows-folder-lists .windows-folder-list");
+var updatefoldernum = document.querySelector(".folder-number-count");
+if(folders.length > 1) {
+    updatefoldernum.innerHTML = folders.length+' items';
+} else {
+    updatefoldernum.innerHTML = folders.length+' item';
+}
 
+
+var winheight = document.querySelector('.windows-header');
+var mainwindows = document.querySelector('.main-windows');
+var winbody = document.querySelector('.windows-body');
+var winheight_and_mainwin = mainwindows.offsetHeight - winheight.offsetHeight;
+winbody.style.height = winheight_and_mainwin+'px';
+
+
+
+
+
+window.onload = function() {
+    initDragElement();
+    initResizeElement();
+  };
+  
+  function initDragElement() {
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    var popups = document.getElementsByClassName("main-windows");
+    var elmnt = null;
+    var currentZIndex = 100; //TODO reset z index when a threshold is passed
+  
+    for (var i = 0; i < popups.length; i++) {
+      var popup = popups[i];
+      var header = getHeader(popup);
+  
+      popup.onmousedown = function() {
+        this.style.zIndex = "" + ++currentZIndex;
+      };
+  
+      if (header) {
+        header.parentPopup = popup;
+        header.onmousedown = dragMouseDown;
+      }
+    }
+  
+    function dragMouseDown(e) {
+      elmnt = this.parentPopup;
+      elmnt.style.zIndex = "" + ++currentZIndex;
+  
+      e = e || window.event;
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      if (!elmnt) {
+        return;
+      }
+  
+      e = e || window.event;
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  
+    function getHeader(element) {
+      var headerItems = element.getElementsByClassName("drag-window");
+  
+      if (headerItems.length === 1) {
+        return headerItems[0];
+      }
+  
+      return null;
+    }
+  }
+  
+  function initResizeElement() {
+    var popups = document.getElementsByClassName("main-windows");
+    var element = null;
+    var startX, startY, startWidth, startHeight;
+  
+    for (var i = 0; i < popups.length; i++) {
+      var p = popups[i];
+  
+      var left = document.createElement("div");
+      left.className = "resizer-left";
+      p.appendChild(left);
+      left.addEventListener("mousedown", initDrag, false);
+      left.parentPopup = p;
+  
+      var right = document.createElement("div");
+      right.className = "resizer-right";
+      p.appendChild(right);
+      right.addEventListener("mousedown", initDrag, false);
+      right.parentPopup = p;
+  
+      var bottom = document.createElement("div");
+      bottom.className = "resizer-bottom";
+      p.appendChild(bottom);
+      bottom.addEventListener("mousedown", initDrag, false);
+      bottom.parentPopup = p;
+  
+      var both = document.createElement("div");
+      both.className = "resizer-both";
+      p.appendChild(both);
+      both.addEventListener("mousedown", initDrag, false);
+      both.parentPopup = p;
+    }
+  
+    function initDrag(e) {
+      element = this.parentPopup;
+  
+      startX = e.clientX;
+      startY = e.clientY;
+      startWidth = parseInt(
+        document.defaultView.getComputedStyle(element).width,
+        10
+      );
+      startHeight = parseInt(
+        document.defaultView.getComputedStyle(element).height,
+        10
+      );
+      document.documentElement.addEventListener("mousemove", doDrag, false);
+      document.documentElement.addEventListener("mouseup", stopDrag, false);
+    }
+  
+    function doDrag(e) {
+      element.style.width = startWidth + e.clientX - startX + "px";
+      element.style.height = startHeight + e.clientY - startY + "px";
+    }
+  
+    function stopDrag() {
+      document.documentElement.removeEventListener("mousemove", doDrag, false);
+      document.documentElement.removeEventListener("mouseup", stopDrag, false);
+    }
+  }
+  
